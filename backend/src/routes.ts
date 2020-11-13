ee84506b6da625316254c223d86a1fd01c21e77c
estevaowat <estevao.watanabe@gmail.com>
2020-11-13 17:29:43 -0300

    add health route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,6 +6,12 @@ const positionController = new PositionController();
 
 routes.get('/paths', positionController.index);
 routes.post('/positions', positionController.get);
+
+routes.get('/health', (req, res) => {
+  res.json({
+    ok: true
+  })
+});
 routes.post('/', positionController.create);
 
 export default routes;
